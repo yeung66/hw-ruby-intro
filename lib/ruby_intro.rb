76267af95f3cbccc1bcf77ecca12485d04fd3975
yeung66 <yeunghl@whu.edu.cn>
2019-08-10 15:58:08 +0000

    finish part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,20 +4,60 @@
 
 def sum arr
   # YOUR CODE HERE
+  ans = 0
+  for i in arr
+    ans = ans + i
+  end
+  ans
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  elsif arr.size==1
+    return arr[0]
+  end
+  max1=arr[0]
+  max2=arr[1]
+  if max1<max2
+    max1,max2=max2,max1
+  end
+  
+  for i in 2..arr.size
+    if arr[i]>max1
+      max1,max2 = arr[i],max1
+    elsif arr[i]>max2
+      max2=arr[i]
+    end
+      
+  end
+  max1+max2
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.size<2
+    return false
+  else
+    dict = Hash.new
+    for i in arr
+      if dict.key? i
+        return true
+      else
+        dict[n-i]=1
+      end
+    end
+  end
+  false
+      
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
