853283f8359de386614b18667bfb0a7f82e39898
yeung66 <yeunghl@whu.edu.cn>
2019-08-11 04:50:56 +0000

    finish hw1 for intro of ruby

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+source 'https://gems.ruby-china.com/'
 ruby '>= 2.2.0', '< 3.0'
 
 gem 'rspec', '~> 3.8'
--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -20,13 +20,21 @@ def max_2_sum arr
   end
   max1=arr[0]
   max2=arr[1]
+  
+  
   if max1<max2
-    max1,max2=max2,max1
+    temp = max1
+    max1 = max2
+    max2 = max1
   end
   
-  for i in 2..arr.size
+  for i in 2..arr.size-1
     if arr[i]>max1
-      max1,max2 = arr[i],max1
+    # if max1<max2
+        # temp = max1
+        
+        max2 = max1
+        max1 = arr[i]
     elsif arr[i]>max2
       max2=arr[i]
     end
@@ -62,14 +70,51 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.empty?
+    return false
+  end
+  aeiou = 'aeiou'
+  temp_s = s.downcase
+  for i in 0..4
+    if temp_s[0] == aeiou[i]
+      return false
+    end
+  end
+  temp_s[0]>='a'&&temp_s[0]<='z'
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  # ans = 0
+  if s.empty?
+    return false
+  end
+  for i in 0..s.size-1
+    if !(s[i]=='0'||s[i]=='1')
+      return false
+    end  
+  end
+  # puts s.to_i(2)
+  s.to_i(2)%4==0
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn,price)
+    if isbn.class!=String || isbn.empty?
+      raise ArgumentError
+    end
+    if price<=0
+      raise ArgumentError
+    end
+    @isbn,@price = isbn,price
+  end
+    
+  attr_accessor :isbn, :price
+  
+  def price_as_string
+    "$%.2f".% @price
+  end
 end
